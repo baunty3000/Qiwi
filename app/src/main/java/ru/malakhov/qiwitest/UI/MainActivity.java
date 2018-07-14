0178f197607dcf3235685d75d3d5498ef0382735
Jay <baunty4000@gmail.com>
2018-07-14 16:58:05 +0300

    Поправил Main

--- app/src/main/java/ru/malakhov/qiwitest/UI/MainActivity.java
+++ app/src/main/java/ru/malakhov/qiwitest/UI/MainActivity.java
@@ -22,43 +22,6 @@ public class MainActivity extends AppCompatActivity implements SpinnerCallBack{
     private ConstructorRecycler mConstrRecycler;
     private Handler mHandler;
 
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d(TAG, "onStart: ");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d(TAG, "onResume: ");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d(TAG, "onPause: ");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d(TAG, "onStop: ");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d(TAG, "onDestroy: ");
-    }
-
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.d(TAG, "onRestart: ");
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
